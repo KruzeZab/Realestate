002fb8cecec0a952e7f879760c23ef8f59cb6be0
kruzezab <kruzezab4@gmail.com>
2023-03-01 14:59:27 +0545

    add carousel on home pae

--- src/components/carousel/ListingCarousel/index.tsx
+++ src/components/carousel/ListingCarousel/index.tsx
@@ -1,5 +1,89 @@
+import { Box, IconButton } from "@chakra-ui/react";
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Listing from "../../Listing";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4,
+    partialVisibilityGutter: 30,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1240 },
+    items: 3,
+    partialVisibilityGutter: 30,
+  },
+  tablet: {
+    breakpoint: { max: 1240, min: 600 },
+    items: 2,
+    partialVisibilityGutter: 30,
+  },
+  mobile: {
+    breakpoint: { max: 600, min: 0 },
+    items: 1,
+    partialVisibilityGutter: 50,
+  },
+};
+
+const LeftArrow = ({ onClick, ...rest }: any) => {
+  // onMove means if dragging or swiping in progress.
+  return (
+    <IconButton
+      display={{ base: "none", md: "flex" }}
+      isRound
+      aria-label="previous listing carousel"
+      icon={<MdArrowBackIos />}
+      position="absolute"
+      colorScheme={"blue"}
+      onClick={onClick}
+    />
+  );
+};
+
+const RightArrow = ({ onClick, ...rest }: any) => {
+  // onMove means if dragging or swiping in progress.
+  return (
+    <IconButton
+      display={{ base: "none", md: "flex" }}
+      isRound
+      aria-label="previous listing carousel"
+      icon={<MdArrowForwardIos />}
+      position="absolute"
+      right="0"
+      colorScheme={"blue"}
+      onClick={onClick}
+    />
+  );
+};
+
 const ListingCarousel = () => {
-  return <div>ListingCarousel</div>;
+  return (
+    <Carousel
+      responsive={responsive}
+      customLeftArrow={<LeftArrow />}
+      customRightArrow={<RightArrow />}
+      partialVisbile
+    >
+      <Box pr={4}>
+        <Listing />
+      </Box>
+      <Box pr={4}>
+        <Listing />
+      </Box>
+      <Box pr={4}>
+        <Listing />
+      </Box>
+      <Box pr={4}>
+        <Listing />
+      </Box>
+      <Box pr={4}>
+        <Listing />
+      </Box>
+    </Carousel>
+  );
 };
 
 export default ListingCarousel;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,39 +3,32 @@ import { Box, Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import ListingCarousel from "../../components/carousel/ListingCarousel";
 import QuickCTA from "../../components/CTA/QuickCTA";
 import Hero from "../../components/Hero";
-import Listing from "../../components/Listing";
 import Testimonial from "../../components/Testimonial";
 import { homeCtas, testimonials } from "../../data";
 
 const HomePage = () => {
   return (
     <>
-      <Box mb={5}>
+      <Box mb={8}>
         <Hero />
       </Box>
       <Container maxW="container.xl">
-        {/* Listings */}
-        <ListingCarousel />
-        <Box mb={5}>
+        <Box mb={8}>
           <Heading textAlign={"center"} size="xl" mb={5}>
             Browse houses near you
           </Heading>
-          <SimpleGrid minChildWidth={"300px"} spacing="20px">
-            <Listing />
-            <Listing />
-            <Listing />
-          </SimpleGrid>
+          <ListingCarousel />
         </Box>
 
         {/* Quick CTA */}
-        <Box mb={5}>
+        <Box mb={8}>
           {homeCtas.map((cta) => (
             <QuickCTA key={cta.link} {...cta} />
           ))}
         </Box>
 
         {/* Testimonials */}
-        <Box mb={5}>
+        <Box mb={8}>
           <Heading textAlign={"center"} size="xl" mb={5}>
             Hear from our Clients
           </Heading>
